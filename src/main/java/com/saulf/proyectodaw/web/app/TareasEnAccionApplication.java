913538322564118dd132f1a1328ccc710a0cd8e1
saulfl54 <saulfl54@educastur.es>
2024-11-09 10:49:12 +0100

    generamos proyecto y configuraciones

---
+++ src/main/java/com/saulf/proyectodaw/web/app/TareasEnAccionApplication.java
@@ -0,0 +1,13 @@
+package com.saulf.proyectodaw.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasEnAccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasEnAccionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saulf/proyectodaw/web/app/TareasEnAccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saulf.proyectodaw.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasEnAccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

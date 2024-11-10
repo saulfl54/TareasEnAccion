2b63c1bc4b316ae9f3a23aaf4184e9a0f30e0bb3
saulfl54 <saulfl54@educastur.es>
2024-11-10 20:20:42 +0100

    creamos las entidades tareas y comentarios y relacionamos las tablas

---
+++ src/main/java/com/saulf/proyectodaw/web/app/models/entity/Comentario.java
@@ -0,0 +1,54 @@
+package com.saulf.proyectodaw.web.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "comentarios")
+public class Comentario implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String contenido;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "create_at")
+	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	private static final long serialVersionUID = 1L;
+	
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,30 @@
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('Paquin','1234','Paco', 'Fernandez', 'paco@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('saulin','1234','Saúl', 'Gutierrez', 'saul@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('marianin','1234','Mariano', 'Dominguez', 'mariano@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('beita','1234','Beatriz', 'Bernabeu', 'bea@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('lore','1234','Lorena', 'Savedra', 'lorena@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('antonin','1234','Antonio', 'Martinez', 'antonio@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('mariita','1234','María', 'Guzman', 'maria@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('adolfin','1234','Adolfo', 'de Lera', 'adolfo@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('vero','1234','Veronica', 'Guzman', 'vero@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('albita','1234','Alba', 'Alvarez', 'alba@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('andresin','1234','Andres', 'Barbas', 'andresr@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('vicentina','1234','Vicenta', 'Fernández', 'vicenta@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('quiquin','1234','Quique', 'Pozo', 'quiquin@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('josin','1234','José', 'Rios', 'jose@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('joaqui','1234','Joaquina', 'Toledana', 'joaquina@proyecto.com');
-INSERT INTO users (username,password, nombre, apellido, email ) VALUES('pili','1234','Pilar', 'Rojas', 'pilar@proyecto.com');
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('Paquin','1234','Paco', 'Fernandez', 'paco@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('saulin','1234','Saúl', 'Gutierrez', 'saul@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('marianin','1234','Mariano', 'Dominguez', 'mariano@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('beita','1234','Beatriz', 'Bernabeu', 'bea@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('lore','1234','Lorena', 'Savedra', 'lorena@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('antonin','1234','Antonio', 'Martinez', 'antonio@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('mariita','1234','María', 'Guzman', 'maria@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('adolfin','1234','Adolfo', 'de Lera', 'adolfo@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('vero','1234','Veronica', 'Guzman', 'vero@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('albita','1234','Alba', 'Alvarez', 'alba@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('andresin','1234','Andres', 'Barbas', 'andresr@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('vicentina','1234','Vicenta', 'Fernández', 'vicenta@proyecto.com');
+
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('quiquin','1234','Quique', 'Pozo', 'quiquin@proyecto.com');
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('josin','1234','José', 'Rios', 'jose@proyecto.com');
+INSERT INTO usuarios (username,password, nombre, apellido, email ) VALUES('joaqui','1234','Joaquina', 'Toledana', 'joaquina@proyecto.com');
+INSERT INTO tareas(usuario_id, titulo ,contenido, create_at) VALUES (1,'Esta es la primera tarea','tomar apuntes!!!','2022-11-01');
+INSERT INTO comentarios (contenido,create_at,tarea_id) VALUES ('primer comentario','2022-11-01',1);
+INSERT INTO comentarios (contenido,create_at,tarea_id) VALUES ('segundo comentario','2022-11-02',1);

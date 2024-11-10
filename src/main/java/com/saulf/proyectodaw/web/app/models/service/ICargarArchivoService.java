4f914d93bf2a329b45db5ab66ae022e454fd7216
saulfl54 <saulfl54@educastur.es>
2024-11-10 17:34:30 +0100

    añadimos subida de imagenes a los usuarios

---
+++ src/main/java/com/saulf/proyectodaw/web/app/models/service/ICargarArchivoService.java
@@ -0,0 +1,25 @@
+package com.saulf.proyectodaw.web.app.models.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * interfaz service  para tratar las imagenes subidas
+ * @author saulf
+ *
+ */
+public interface ICargarArchivoService {
+
+	public Resource load(String filename) throws MalformedURLException;
+
+	public String copy(MultipartFile file) throws IOException;
+
+	public boolean delete(String filename);
+
+	public void deleteAll();
+
+	public void init() throws IOException;
+}
